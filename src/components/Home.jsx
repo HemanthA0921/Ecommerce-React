42b0e9f41497d7602c0236b5bc1e043178979872
HemanthA0921 <hemanthadigopula0921@gmail.com>
2023-09-29 11:24:53 +0530

    added category section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ import { ShopCategories } from "./ShopCategories";
 import { HomeBestDeals } from "./HomeBestDeals";
 import { BrandSection } from "./BrandSection";
 import { PopularProducts } from "./PopularProducts";
+import { HomeCategorySection } from "./HomeCategorySection";
 import "../styles/Home.css";
 
 export const Home = () => {
@@ -18,6 +19,7 @@ export const Home = () => {
             <HomeBestDeals />
             <BrandSection />
             <PopularProducts />
+            <HomeCategorySection />
             <Footer />
         </div>
     );
