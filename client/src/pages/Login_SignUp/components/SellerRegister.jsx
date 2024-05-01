ad259aba50028882de0af5d4da5e835e839484ae
HemanthA0921 <97177861+HemanthA0921@users.noreply.github.com>
2024-05-01 22:41:15 +0530

    Update SellerRegister.jsx

--- client/src/pages/Login_SignUp/components/SellerRegister.jsx
+++ client/src/pages/Login_SignUp/components/SellerRegister.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate, Link } from 'react-router-dom';
 import axios from "axios";
-import "../styles/login.css";
+import "../styles/Login.css";
 
 export const SellerRegister = () => {
     const [formData, setFormData] = useState({
