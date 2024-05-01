360b61af743461a3caabddfaf451af7b8fe4e048
HemanthA0921 <97177861+HemanthA0921@users.noreply.github.com>
2024-05-01 22:38:24 +0530

    Update Login.jsx

--- client/src/pages/Login_SignUp/components/Login.jsx
+++ client/src/pages/Login_SignUp/components/Login.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import axios from "axios";
-import "../styles/login.css";
+import "../styles/Login.css";
 
 export const Login = ({ setLoginUser }) => {
     const navigate = useNavigate();
